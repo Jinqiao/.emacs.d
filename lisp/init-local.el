651fd159e47510692739e2cc43b35cfa79ac9f98
jinqiao <lijinqiao222@gmail.com>
2019-05-01 14:07:28 -0400

    comment out proxy settings

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,6 +1,8 @@
+
+;; only needed below company proxy
 ;; proxy https://stackoverflow.com/a/18697151/4499942
-(setq url-proxy-services
-   '(("http" . "10.50.62.14:8080")
-     ("https" . "10.50.62.14:8080")))
+;; (setq url-proxy-services
+;;    '(("http" . "10.50.62.14:8080")
+;;      ("https" . "10.50.62.14:8080")))
 
 (provide 'init-local)
