4a27d408a402489cebdf7c9cdf249b6075ae66b2
jinqiao <lijinqiao222@gmail.com>
2019-05-01 11:40:05 -0400

    add package list in init-elpa

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -3,4 +3,17 @@
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
+
+;; refresh contents if package-user-dir not exists
+(or
+ (file-exists-p package-user-dir)
+ (package-refresh-contents))
+
+;; install the missing packages
+;; https://stackoverflow.com/a/10093312/4499942
+(setq package-list '(company beacon counsel elpy expand-region flycheck move-dup multiple-cursors symbol-overlay undo-tree))
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
 (provide 'init-elpa)
