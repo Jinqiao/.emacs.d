bf890dd9b0298e2141f5c787a9ebb31b01d6b01e
jq <lijinqiao222@gmail.com>
2019-05-03 21:45:42 -0400

    edit init-python

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 ;; load custom https://stackoverflow.com/a/5058752/4499942
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq custom-file "~/.emacs.d/custom.el")
 
 ;; add the lisp dir to load-path
--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,5 +1,12 @@
 ;; elpy
 (elpy-enable)
 
+;; use ipython
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
+
+;; disable indention highlight
+(add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
+
 ;; end
 (provide 'init-python)
